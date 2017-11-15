7d2bc147d58cf7671a10f87d3eaf8409f4f8c87d
Hendrik <drikdoank@gmail.com>
2017-11-15 17:46:49 +0700

    video

---
+++ protected/views/Video/_thumb.php
@@ -0,0 +1,8 @@
+
+<li style="width:240px; height:180px;">
+	<a href="http://localhost/siatab/data//data/Unit Kerja/Balai Wilayah Sungai Bali Penida/Mata Air/Video/<?php echo $data->video ?>" class="thumbnail" rel="tooltip" data-title="video">
+		<video width="230" height="180" controls id="video">
+    	<source src="http://localhost/siatab/data/Unit Kerja/Balai Wilayah Sungai Bali Penida/Mata Air/Video/<?php echo $data->video ?>" type="video/mp4">
+		</video>
+	</a>
+</li>
