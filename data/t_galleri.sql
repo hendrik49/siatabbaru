ea2952635a855d89fcc417838403c7ef2fe8ac32
Hendrik <drikdoank@gmail.com>
2017-11-24 10:24:27 +0700

    "tabel baru"

--- data/t_galleri.sql
+++ data/t_galleri.sql
@@ -23,13 +23,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_video`
+-- Table structure for table `t_galleri`
 --
 
-CREATE TABLE IF NOT EXISTS `t_video` (
+CREATE TABLE IF NOT EXISTS `t_galleri` (
 `ID` int(11) NOT NULL,
   `Kategori` varchar(255) NOT NULL,
-  `NamaVideo` varchar(255) NOT NULL,
+  `NamaGambar` varchar(255) NOT NULL,
   `Tanggal` int(11) NOT NULL,
   `status` int(2) NOT NULL,
   `Link` varchar(255) NOT NULL,
@@ -37,22 +37,22 @@ CREATE TABLE IF NOT EXISTS `t_video` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 --
--- Dumping data for table `t_video`
+-- Dumping data for table `t_galleri`
 --
 
-INSERT INTO `t_video` (`ID`, `Kategori`, `NamaVideo`, `Tanggal`, `status`, `Link`, `Deskripsi`) VALUES
-(2, 'Sumur', 'Sumur Tahap 1', 1502120152, 1, '20161214_174624.mp4', '<p>Sebuah Video Tahap 1</p>\r\n'),
-(3, 'Sumur', 'Sumur Tahap 2', 1502120166, 1, '20161214_174624.mp4', '<p>Sumur Tahap 2</p>\r\n'),
-(4, 'Sumur', 'Sumur Tahap 3', 1502121180, 1, '20161214_174624.mp4', '<p>Sumur Tahap 3</p>\r\n');
+INSERT INTO `t_galleri` (`ID`, `Kategori`, `NamaGambar`, `Tanggal`, `status`, `Link`, `Deskripsi`) VALUES
+(2, 'Sumur', 'Pembangunan Sumur di Bali', 1502120152, 1, '20161128_143108.jpg', '<p>Pembangunan Sumur di Bali</p>\r\n'),
+(3, 'Sumur', 'Pembangunan Sumur Tahap 2', 1502120166, 1, '20161128_143304.jpg', '<p>Pembangunan Sumur Tahap 2</p>\r\n'),
+(4, 'Sumur', 'Pembangunan Sumur Tahap 3', 1502121180, 1, '20161128_143121.jpg', '<p>Pembangunan Sumur Tahap 3</p>\r\n');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `t_video`
+-- Indexes for table `t_galleri`
 --
-ALTER TABLE `t_video`
+ALTER TABLE `t_galleri`
  ADD PRIMARY KEY (`ID`);
 
 --
@@ -60,9 +60,9 @@ ALTER TABLE `t_video`
 --
 
 --
--- AUTO_INCREMENT for table `t_video`
+-- AUTO_INCREMENT for table `t_galleri`
 --
-ALTER TABLE `t_video`
+ALTER TABLE `t_galleri`
 MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
